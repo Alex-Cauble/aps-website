f12fe54ec635b5867cfe46aca1a794debf6749ca
Alex-Cauble <alexander.t.cauble@gmail.com>
2021-02-24 09:44:20 -0600

    refactor: removed old commented out code from refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// import { AustinHighInfoComponent } from './component/austin-high-info/austin-high-info.component';
-// import { BanfieldInfoComponent } from './component/banfield-info/banfield-info.component';
 import { BidsComponent } from './component/bids/bids.component';
-import { ContactComponent } from './component/building-contact/contact.component';
+import { ContactComponent } from './component/building-contact/building-contact.component';
 import { CalendarsComponent } from './component/calendars/calendars.component';
-// import { ClcInfoComponent } from './component/clc-info/clc-info.component';
-// import { EllisInfoComponent } from './component/ellis-info/ellis-info.component';
 import { HomeComponent } from './component/home/home.component';
-// import { IjholtonInfoComponent } from './component/ijholton-info/ijholton-info.component';
-// import { NevelnInfoComponent } from './component/neveln-info/neveln-info.component';
 import { SchoolBoardComponent } from './component/school-board/school-board.component';
-// import { SouthgateInfoComponent } from './component/southgate-info/southgate-info.component';
-// import { SumnerInfoComponent } from './component/sumner-info/sumner-info.component';
-// import { WoodsonInfoComponent } from './component/woodson-info/woodson-info.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
